0723ce57c6ed2d13a7641cbe65de554794283ef0
Shivani21-hude <shivani21hude@gmail.com>
2023-02-01 10:18:44 +0530

    [Shivani]-Add List,Stack,Set,Queue,Dictionary using collection

---
+++ CollectionProblem/Collection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionProblem
+{
+    public class Collection
+    {
+        public static void List()
+        {
+            Console.WriteLine("\n ListDemo....");
+            List<string> list = new List<string>();
+            list.Add("Pranay");
+            list.Add("Vijay");
+            list.Add("Ravi");
+            list.Add("Ajay");
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+
+            }
+
+        }
+
+        public static void Stack()
+        {
+
+            Console.WriteLine("\n Stack Demo....");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Sarang");
+            stack.Push("Sita");
+            stack.Push("Laxman");
+            stack.Push("Devki");
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void Set()
+        {
+            Console.WriteLine("\n Set Demo....");
+            var set = new HashSet<string>();
+            set.Add("Shubham");
+            set.Add("arya");
+            set.Add("vicky");
+            set.Add("lucky");
+
+            foreach (var element in set)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void Queue()
+        {
+            Console.WriteLine("\n Queue Demo.....");
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Sham");
+            queue.Enqueue("vish");
+            queue.Enqueue("sayu");
+            queue.Enqueue("Sima");
+
+            foreach (var element in queue)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void Dictionary()
+        {
+            Console.WriteLine("\n Dictionary Demo....");
+
+            Dictionary<int, string> dictionory = new Dictionary<int, string>();
+            dictionory.Add(100, "ram");
+            dictionory.Add(200, "rahul");
+            dictionory.Add(300, "Mani");
+            dictionory.Add(400, "Vrutik");
+
+            foreach (var element in dictionory)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ CollectionProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CollectionProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine( "***** Welcome to Collection Ligrary ***** ");
+            Collection.List();
+            Collection.Stack();
+            Collection.Set();
+            Collection.Queue();
+            Collection.Dictionary();
+        }
+    }
+}
